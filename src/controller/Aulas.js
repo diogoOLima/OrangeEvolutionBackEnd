ffe4681b68b6753394d1c1410cbee7a0eb41aa37
Lucas_Passos <lucas_passos@msn.com>
2022-11-07 10:34:08 -0300

    Criacao da pasta controllers e seus devidos arquivos js de controle para cada entidade

---
+++ src/controller/Aulas.js
@@ -0,0 +1,70 @@
+import aulasModel from "../models/aulasModel.js";
+import databaseAulas from "../DAO/databaseAulas.js";
+
+databaseAulas.criarTabelaAulas();
+
+class Aulas {
+    
+    //listar todas aulas
+    static rotas(app) {                                  
+        app.get("/aulas", async (req, res)=>{
+            const resposta = await databaseAulas.listarTodasAulas();
+            res.status(200).json(resposta)
+        });
+
+        //listar por id
+        app.get("/aulas/:id", async(req, res) => {
+            try {
+                const aulas = await databaseAulas.listarAulasPorId(req.params.id);
+                if(!aulas) {
+                    throw new Error("Aula não encontrada para este Id!")
+                }
+                res.status(200).json(aulas);
+            } catch(error) {
+                res.status(404).json(error.message);
+            }
+        });
+
+        //inserir aula
+        app.post("/aulas", async(req, res)=> {
+            try {
+                const aulas = new aulasModel(...Object.values(req.body));
+                const resposta = await databaseAulas.inserirAula(aulas);
+                res.status(201).json(resposta);
+            } catch(error) {
+                res.status(400).json(error.message);
+            }
+        });
+
+        //atualizar por id
+        app.put("/aulas/:id", async(req,res)=> {
+            try {
+                const aulaId = await databaseAulas.listarAulasPorId(req.params.id);
+                if(!aulaId) {
+                    throw new Error("Aula não encontrada para esse Id.")
+                }
+                const aulas = new aulasModel(...Object.values(req.body));
+                const resposta = await databaseAulas.atualizarAulaPorId(req.params.id, aulas);
+                res.status(200).json(resposta);
+            } catch (error) {
+                res.status(400).json(error.message);
+              }
+        });
+
+        //deletar por id
+        app.delete("/aulas/:id", async(req,res)=> {
+            try {
+                const aulaId = await databaseAulas.listarAulasPorId(req.params.id);
+                if(!aulaId) {
+                    throw new Error("Aula não encontrada para esse Id.")
+                }
+                const aulas = await databaseAulas.deletarAulaPorId(req.params.id);
+                res.status(200).json(aulas);
+            }catch (error) {    
+                res.status(404).json({ Error: error.message });
+              }
+        });
+    }
+}
+
+export default Aulas;
---
+++ src/controller/Usuarios.js
@@ -0,0 +1,76 @@
+import usuariosModel from "../models/usuariosModel.js";
+import databaseUsuarios from "../DAO/databaseUsuarios.js";
+
+databaseUsuarios.criarTabelaUsuarios();
+
+class Usuarios {
+    
+    //listar todos usuarios
+    static rotas(app) {                                  
+        app.get("/usuarios", async (req, res)=>{
+            try {
+                const resposta = await databaseUsuarios.listarTodosUsuarios();
+                res.status(200).json(resposta);
+            } catch(error) {
+                res.status(404).json(error.message);
+                console.log("erro em get")
+            }
+            
+        });
+
+        //listar por id
+        app.get("/usuarios/:id", async(req, res) => {
+            try {
+                const usuario = await databaseUsuarios.listarUsuarioPorId(req.params.id);
+                if(!usuario) {
+                    throw new Error("Usuário não encontrado para este Id!")
+                }
+                res.status(200).json(usuario);
+            } catch(error) {
+                res.status(404).json(error.message);
+            }
+        });
+
+        //inserir usuario
+        app.post("/usuarios", async(req, res)=> {
+            try {
+                const usuario = new usuariosModel(...Object.values(req.body));
+                const resposta = await databaseUsuarios.inserirUsuario(usuario);
+                res.status(201).json(resposta);
+            } catch(error) {
+                res.status(400).json(error.message)
+            }
+        });
+
+        //atualizar por id
+        app.put("/usuarios/:id", async(req,res)=> {
+            try {
+                const usuarioId = await databaseUsuarios.listarUsuarioPorId(req.params.id);
+                if(!usuarioId) {
+                    throw new Error("Usuário não encontrado para esse Id.")
+                }
+                const usuario = new usuariosModel(...Object.values(req.body));
+                const resposta = await databaseUsuarios.atualizarUsuarioPorId(req.params.id, usuario);
+                res.status(200).json(resposta);
+            } catch (error) {
+                res.status(400).json(error.message);
+              }
+        });
+
+        //deletar por id
+        app.delete("/usuarios/:id", async(req,res)=> {
+            try {
+                const usuarioId = await databaseUsuarios.listarUsuarioPorId(req.params.id);
+                if(!usuarioId) {
+                    throw new Error("Usuário não encontrado para esse Id.")
+                }
+                const usuario = await databaseUsuarios.deletarUsuarioPorId(req.params.id);
+                res.status(200).json(usuario);
+            }catch (error) {    
+                res.status(404).json({ Error: error.message });
+              }
+        });
+    }
+}
+
+export default Usuarios;
