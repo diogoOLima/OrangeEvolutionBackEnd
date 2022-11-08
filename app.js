de7896ba435ada9a8343ad5638a3d8f64429517f
Lucas_Passos <lucas_passos@msn.com>
2022-11-08 10:21:01 -0300

    Adicao de funcao de utilizacao da biblioteca favicon

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 import express  from "express";
+import expressFavicon from "express-favicon";
 import * as dotenv from "dotenv";
 import Usuarios from "./src/controller/Usuarios.js";
 import Trilhas from "./src/controller/Trilhas.js";
@@ -7,13 +8,15 @@ import AulasAcessadas from './src/controller/AulasAcessadas.js'
 
 dotenv.config();
 
+const favicon = require(expressFavicon);
 const port = process.env.PORT || 3001
-const app = express()
+const app = express();
 
 app.listen(port, ()=> {
     console.log(`Servidor rodando em http://localhost:${port}`)
 });
 
+app.use(favicon(__dirname + 'public/favicon.png'));
 app.use(express.json());
 
 Usuarios.rotas(app);
